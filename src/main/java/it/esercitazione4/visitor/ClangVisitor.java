8d4b8344607ec9cf92440ebad49e746b53f74c64
= <gerardo.gullo@gmail.com>
2021-01-03 15:48:42 +0100

    Add ClangVisitor

---
+++ src/main/java/it/esercitazione4/visitor/ClangVisitor.java
@@ -0,0 +1,191 @@
+package it.esercitazione4.visitor;
+
+import it.esercitazione4.nodes.AssignStatNode;
+import it.esercitazione4.nodes.BoolConstLeaf;
+import it.esercitazione4.nodes.CallProcNode;
+import it.esercitazione4.nodes.ElifListNode;
+import it.esercitazione4.nodes.ElifNode;
+import it.esercitazione4.nodes.ElseNode;
+import it.esercitazione4.nodes.ExprListNode;
+import it.esercitazione4.nodes.ExprNode;
+import it.esercitazione4.nodes.FloatConstLeaf;
+import it.esercitazione4.nodes.IdLeaf;
+import it.esercitazione4.nodes.IdListInitNode;
+import it.esercitazione4.nodes.IdListNode;
+import it.esercitazione4.nodes.IfStatNode;
+import it.esercitazione4.nodes.IntConstLeaf;
+import it.esercitazione4.nodes.NullConstLeaf;
+import it.esercitazione4.nodes.ParDeclNode;
+import it.esercitazione4.nodes.ParamDeclListNode;
+import it.esercitazione4.nodes.ProcListNode;
+import it.esercitazione4.nodes.ProcNode;
+import it.esercitazione4.nodes.ProgramNode;
+import it.esercitazione4.nodes.ReadlnStatNode;
+import it.esercitazione4.nodes.ResultTypeListNode;
+import it.esercitazione4.nodes.ResultTypeNode;
+import it.esercitazione4.nodes.ReturnExprsNode;
+import it.esercitazione4.nodes.StatListNode;
+import it.esercitazione4.nodes.StringConstLeaf;
+import it.esercitazione4.nodes.TypeDeclNode;
+import it.esercitazione4.nodes.VarDeclListNode;
+import it.esercitazione4.nodes.VarDeclNode;
+import it.esercitazione4.nodes.WhileStatNode;
+import it.esercitazione4.nodes.WriteStatNode;
+
+public class ClangVisitor implements Visitor{
+
+  @Override
+  public Object visit(AssignStatNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(BoolConstLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(CallProcNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ElifListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ElifNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ElseNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ExprListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ExprNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(FloatConstLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(IdLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(IdListInitNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(IdListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(IfStatNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(IntConstLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(NullConstLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ParamDeclListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ParDeclNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ProcListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ProcNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ProgramNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ReadlnStatNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ResultTypeListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ResultTypeNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(ReturnExprsNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(StatListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(StringConstLeaf leaf) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(TypeDeclNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(VarDeclListNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(VarDeclNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(WhileStatNode node) throws Exception {
+    return null;
+  }
+
+  @Override
+  public Object visit(WriteStatNode node) throws Exception {
+    return null;
+  }
+}
